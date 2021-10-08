7fa8f2e57f8d41359089117e7fe1c5ca8383ed18
thanhnht <thanhnht@fsoft.com.vn>
2021-10-08 20:28:08 +0700

    Client dockerizing

--- Bug.Client/src/app/app-routing.module.ts
+++ Bug.Client/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
+import { TestComponent } from './test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./modules/authencation/authencation.module').then(mod => mod.AuthencationModule)
+  },
+  { path: 'test', component: TestComponent },
+  { path: '404', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/' + '404' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    scrollPositionRestoration: 'enabled',
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
